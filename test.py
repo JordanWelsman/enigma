63e517f05b89b3007ab44f0067fac81515f95d68
Jordan Welsman <jordan.welsman@hotmail.co.uk>
2024-01-13 19:41:01 +0000

    test: Tested Plugboard class.

--- test.py
+++ test.py
@@ -5,15 +5,5 @@ from rotors import Rotor
 
 # Test Environment
 p = Plugboard([['J', 'O'], ['R', 'D'], ['A', 'N']])
-r = Rotor()
-
-# string = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG"
-# print(f"Raw:      {string}")
-
-# encoded = r.encode(string, " ")
-# print(f"Encoded:  {encoded}")
-
-# decoded = r.decode(encoded, " ")
-# print(f"Decoded:  {decoded}")
 
 print(p('JORDAN'))
