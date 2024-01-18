b20aaf8eb82ed97a9d7161a1eacd69a83189bc6a
Jordan Welsman <jordan.welsman@hotmail.co.uk>
2024-01-18 21:13:46 +0000

    test: Key and Rotor test.

--- test.py
+++ test.py
@@ -1,9 +1,32 @@
 # Module Imports
+from keys import Key, reflector_configurations, rotor_configurations
 from plugboard import Plugboard
+from random import seed, shuffle
 from rotors import Rotor
+from time import sleep
 
 
 # Test Environment
-p = Plugboard([['J', 'O'], ['R', 'D'], ['A', 'N']])
+k = Key('I')
+r = Rotor(k)
+print(k)
+print(r)
 
-print(p('JORDAN'))
+
+# rotor_1 = 0
+# rotor_2 = 0
+# rotor_3 = 0
+
+# for _ in range(10_000):
+#     sleep(0.01)
+#     rotor_1 += 1
+#     if rotor_1 == 26:
+#         rotor_1 = 0
+#         rotor_2 += 1
+#         if rotor_2 == 26:
+#             rotor_2 = 0
+#             rotor_3 += 1
+#             if rotor_3 == 26:
+#                 rotor_3 = 0
+
+#     print(f'{rotor_1}, {rotor_2}, {rotor_3}')
